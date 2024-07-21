9f2ff332bf83e2a71163be1637c104a2c8d49722
Usman <usman17.s@hotmail.com>
2024-07-21 20:56:28 +0100

    Add basic terms adn conditions

--- src/drug_discovery/urls.py
+++ src/drug_discovery/urls.py
@@ -22,7 +22,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', views.home, name="home"),
+    path('privacy_policy', views.privacy_policy, name="privacy_policy"),
+    path('terms_and_conditions', views.terms_and_conditions, name="terms_and_conditions"),
+  
+    
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
 
--- src/drug_discovery/views.py
+++ src/drug_discovery/views.py
@@ -4,3 +4,11 @@ def home(request):
 
     return render(request, 'home_site/index.html')
 
+
+def privacy_policy(request):
+    return render(request, 'home_site/privacy_policy.html')
+
+
+
+def terms_and_conditions(request):
+    return render(request, 'home_site/terms_and_conditions.html')
